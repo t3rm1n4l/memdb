02ae8e38d0e0dacdbc5269ec35723b8912844bc2
Sarath Lakshman <sarathlakshman@slynux.com>
2016-04-13 12:11:21 +0530

    memdb: Implement block manager for batchops

--- memdb_test.go
+++ memdb_test.go
@@ -25,10 +25,6 @@ func TestBatchOps(t *testing.T) {
 	db := NewWithConfig(testConf)
 	defer db.Close()
 
-	w, err := db.NewDiskWriter()
-	if err != nil {
-		panic(err)
-	}
 	n := 1000000
 
 	var snap *Snapshot
@@ -47,7 +43,7 @@ func TestBatchOps(t *testing.T) {
 		}
 
 		t0 := time.Now()
-		fmt.Println(w.BatchModify(ops))
+		fmt.Println(db.BatchModify(ops))
 		fmt.Println("thr", float64(n)/float64(time.Since(t0).Seconds()))
 		if snap != nil {
 			snap.Close()
